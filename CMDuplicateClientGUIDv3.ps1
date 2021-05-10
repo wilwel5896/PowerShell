d30cbb8825a792725b625d5a80b99fc21ac67a90
wilwel5896 <wilwel5896@live.com>
2021-05-10 14:45:35 -0500

    Update CMDuplicateClientGUIDv3.ps1

--- CMDuplicateClientGUIDv3.ps1
+++ CMDuplicateClientGUIDv3.ps1
@@ -1,25 +1,4 @@
-################################
-#                              #
-#         DISCLAIMER           #
-#                              #
-################################
-
-
 <#
-
-The following script is not supported under any Microsoft standard support program or service. 
-The following script is provided AS IS without warranty of any kind. Microsoft further disclaims
-all implied warranties including, without limitation, any implied warranties of merchantability 
-or of fitness for a particular purpose. The entire risk arising out of the use or performance 
-of the following script and documentation remains with you. In no event shall Microsoft, its authors, 
-or anyone else involved in the creation, production, or delivery of the scripts be liable for 
-any damages whatsoever (including, without limitation, damages for loss of business profits,
-business interruption, loss of business information, or other pecuniary loss) arising out of the
-use of or inability to use the following script or documentation, even if Microsoft has been 
-advised of the possibility of such damages.
-
-© 2021 Microsoft. All rights reserved.
-
     .SCRIPTNAME 
         CMDuplicateClientGUID.ps1
 
